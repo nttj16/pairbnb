405aa3d1edc239d32e8649ad771a3d1c8f43ff69
nttj16 <nttj16@gmail.com>
2017-03-29 10:38:02 +0800

    added seeds to the file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Seed Users
+user = {}
+user['password'] = "password"
+
+ActiveRecord::Base.transaction do
+	20.times do
+		user[:name] = Faker::StarWars.character
+		user[:email] = Faker::Internet.email
+
+
+		User.create(user)
+	end
+end
+
+# Seed Listings
+listing = {}
+uids = []
+User.all.each { |u| uids << u.id }
+
+ActiveRecord::Base.transaction do
+  40.times do
+    listing['title'] = Faker::App.name
+    listing['country'] = Faker::Address.country
+    listing['state'] = Faker::Address.state
+    listing['city'] = Faker::Address.city
+    listing['price'] = rand(80..500)
+    listing['description'] = Faker::Hipster.sentence
+
+    listing['user_id'] = uids.sample
+
+    Listing.create(listing)
+end
+end
